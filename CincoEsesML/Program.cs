60b8ecdc779450125bc6e6a38febea176aa6471c
manimanitin <andreman1781@gmail.com>
2023-11-11 07:28:47 -0600

    Agregar archivos de proyecto.

---
+++ CincoEsesML/Program.cs
@@ -0,0 +1,15 @@
+﻿//Load sample data
+using CincoEsesML;
+
+string _assetsPath = Path.Combine(Environment.CurrentDirectory, "assets");
+var imageBytes = File.ReadAllBytes($"{_assetsPath}/cavi.jpg");
+QualityModel.ModelInput sampleData = new QualityModel.ModelInput()
+{
+    ImageSource = imageBytes,
+};
+
+//Load model and predict output
+var result = QualityModel.Predict(sampleData);
+
+Console.WriteLine("\n" + result.PredictedLabel);
+
