f07d5594bf455d21a48164b5aa3321b008b5d116
manimanitin <andreman1781@gmail.com>
2023-11-11 11:04:52 -0600

    Agregar la salida del modelo

---
+++ Pages/FileByteReader.cs
@@ -0,0 +1,34 @@
+﻿namespace Pages.Pages
+{
+    public class FileByteReader
+    {
+        public byte[] ReadBytesFromFile(string filePath)
+        {
+            try
+            {
+                // Check if the file exists
+                if (File.Exists(filePath))
+                {
+                    // Read all bytes from the file
+                    byte[] fileBytes = File.ReadAllBytes(filePath);
+
+                    // You now have the bytes of the file in the 'fileBytes' array
+                    return fileBytes;
+                }
+                else
+                {
+                    // Handle the case where the file does not exist
+                    Console.WriteLine("File not found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle exceptions that may occur during file reading
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            // Return null if there was an issue reading the file
+            return null;
+        }
+    }
+}
--- Pages/Pages/Index.cshtml.cs
+++ Pages/Pages/Index.cshtml.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.ML;
 using static CincoEsesML.QualityModel;
+using System;
+using System.IO;
 
 namespace Pages.Pages
 {
@@ -9,6 +11,15 @@ namespace Pages.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly PredictionEnginePool<ModelInput, ModelOutput> _predictionEnginePool;
+
+        [BindProperty]
+        public IFormFile ImageFile { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string localImagePath { get; set; }
+
+        public string calidad { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger, PredictionEnginePool<ModelInput, ModelOutput> predictionEnginePool)
         {
@@ -18,17 +29,63 @@ namespace Pages.Pages
 
         public void OnGet()
         {
+            ImagePath = TempData["ImagePath"] as string;
+            Console.WriteLine(TempData["ImagePath"]);
+            localImagePath = TempData["localImagePath"] as string;
+            Console.WriteLine(localImagePath);
+            if (localImagePath != null)
+            {
+                FileByteReader fileReader = new FileByteReader();
+                byte[] bytes = fileReader.ReadBytesFromFile(localImagePath);
+                if (bytes != null)
+                {
+                    OnGetAnalyzeSentiment(bytes);
+                    calidad = TempData["calidad"] as string;
+                    Console.WriteLine(calidad);
 
+                }
+
+            }
         }
 
-        public IActionResult OnGetAnalyzeSentiment(byte[] imageBytes)
+        public void OnGetAnalyzeSentiment(byte[] imageBytes)
         {
-            if (imageBytes.Length == 0) return Content("Neutral");
+            if (imageBytes.Length == 0)
+            {
+                TempData["calidad"] = "Neutral";
+                return;
+            }
+
             var input = new ModelInput { ImageSource = imageBytes };
             var prediction = _predictionEnginePool.Predict(input);
             var sentiment = prediction.PredictedLabel == "Seguro" ? "Seguro" : "No Seguro";
-            return Content(sentiment);
+            TempData["calidad"] = sentiment;
+
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (ImageFile != null && ImageFile.Length > 0)
+            {
+                // Specify the path where you want to save the uploaded image
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "uploads", ImageFile.FileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ImageFile.CopyToAsync(stream);
+                }
+
+                // Optional: Add logic to save the file path to a database or perform other actions
+                TempData["localImagePath"] = filePath;
+                TempData["ImagePath"] = $"https://localhost:7241/uploads/{ImageFile.FileName}";
+
+                return RedirectToPage("/Index"); // Redirect to another page after successful upload
+            }
+
+            ModelState.AddModelError("ImageFile", "Please select a file to upload.");
+            return Page();
+        }
+
 
     }
 }
